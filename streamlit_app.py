0b900265d4e841a70fbc686aa240e875cf9c2bd3
Prasad H <113333035+prasadharake@users.noreply.github.com>
2022-09-12 13:47:23 +0530

    Update streamlit_app.py
    Error

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,5 @@
 import streamlit as  st
-import Pandas as pd
+import Pandas 
 
 st.title('My MOMs New Healthy Diner')
 st.header('Breakfast Menu')
@@ -11,5 +11,5 @@ st.text('🥑🍞Avocado Toast')
    
 st.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
-my_fruit_list = pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 st.dataframe(my_fruit_list)
