2fb2718ec4d3198f018106469e2486556332968b
Prasad H <113333035+prasadharake@users.noreply.github.com>
2022-09-12 13:16:28 +0530

    Update streamlit_app.py
    Fixed error in text

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,6 @@ import streamlit as  st
 
 st.title('My Parents New Healthy Diner')
 st.subheader('Breakfast Menu ')
-st.text('1.Pizza
-        2.Burger
+st.text('1.Pizza \n
+        2.Burger\n
         3.Fries')
