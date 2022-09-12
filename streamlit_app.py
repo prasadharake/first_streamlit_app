4eb49e98f642a9d7e8cc2f15c01bcea6b360d680
Prasad H <113333035+prasadharake@users.noreply.github.com>
2022-09-12 13:46:03 +0530

    Update streamlit_app.py
    Error debugging

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,5 +11,5 @@ st.text('🥑🍞Avocado Toast')
    
 st.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
-my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
-streamlit.dataframe(my_fruit_list)
+my_fruit_list = pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+st.dataframe(my_fruit_list)
