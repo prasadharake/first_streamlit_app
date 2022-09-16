69accee396bd8c3f19f2b5016fc960223a5b1ffd
Prasad H <113333035+prasadharake@users.noreply.github.com>
2022-09-16 10:30:53 +0530

    Update streamlit_app.py
    Error resolved in new section

--- streamlit_app.py
+++ streamlit_app.py
@@ -26,4 +26,4 @@ st.dataframe(fruits_to_show)
 import requests
 streamlit.header("Fruityvice Fruit Advice!")
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response.json())
+st.text(fruityvice_response.json())
